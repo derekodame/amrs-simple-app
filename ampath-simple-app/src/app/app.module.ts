db8fb9faa7a85c3efed93fa36bd0e9b65307f91a
derek odame <dodame@localhost.localdomain>
2019-04-10 23:14:39 -0400

    update

--- ampath-simple-app/src/app/app.module.ts
+++ ampath-simple-app/src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatMenuModule} from '@angular/material/menu'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table'; 
+
+
+
 
 @NgModule({
   declarations: [
@@ -12,11 +21,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MatToolbarModule,
-    BrowserAnimationsModule
-
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatMenuModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
